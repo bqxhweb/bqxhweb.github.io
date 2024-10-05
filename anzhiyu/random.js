d037625c9aad79b77180b3170eb2e51f929c5ff6
bqxhweb <3200232561@qq.com>
2024-10-05 13:11:49 +0800

    Site updated: 2024-10-05 13:11:48

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/05/hello-world/"];function toRandomPost(){
+var posts=["2024/08/10/Tuta/","2024/08/06/Tox/","2024/05/19/注册GitHub账号及评论区的使用/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
